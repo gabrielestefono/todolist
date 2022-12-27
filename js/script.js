a0bfe584378b11b7d757867ca10922d164e85f00
Gabriel Estéfono <gabrielestefono@hotmail.com>
2022-12-27 09:45:53 -0300

    27/12/2022 4º Commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 let input = document.querySelector("input[name=tarefa]");
 let btn = document.querySelector("#botao");
 let lista = document.querySelector("#lista");
+let card = document.querySelector(".card");
 
-let tarefas = [
-    "Tarefa 01",
-    "Tarefa 02",
-    "Tarefa 03",
-];
+let tarefas = [];
 
-function rederizarTarefas(){
+function renderizarTarefas(){
     for(tarefa of tarefas){
         let itemLista = document.createElement("li");
         itemLista.setAttribute("class", "list-group-item list-group-item-action");
@@ -18,10 +15,34 @@ function rederizarTarefas(){
     };
 };
 
-function adicionarLista(){
-    tarefas.push(input.value);
+renderizarTarefas();
+
+btn.addEventListener('click', ()=>{
+    let novaTarefa = input.value;
+    if(novaTarefa !== ''){
+    lista.innerHTML = '';
+    tarefas.push(novaTarefa);
+    renderizarTarefas();
+    input.value = '';
+    removerSpans();
+    }
+    else{
+        removerSpans();
+        let span = document.createElement("span");
+        span.setAttribute("class", "alert alert-warning");
+        let msg = document.createTextNode("Você precisa informar a tarefa!");
+        span.appendChild(msg);
+        card.appendChild(span);
+    }
+});
+
+function removerSpans(){
+    let spans = document.querySelectorAll('span');
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i]);
+    };
+};
+
+function deletarTarefa(tarefa){
+
 }
-
-rederizarTarefas();
-
-btn.onclick = adicionarLista;
